5f5c29a3d630c5a6f228653eacd6d89a8d0737a9
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-09-01 23:27:38 -0300

    added 3d object

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import "./App.css"
+import Object from "./components/object/Object"
+
+function App() {
+  return (
+    <>
+      <Object />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/object/Object.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useRef, useEffect } from "react"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { OBJLoader } from "three/addons/loaders/OBJLoader.js"
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
+import "./object.css"
+
+const Object = () => {
+  const canvasRef = useRef(null)
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  const controls = new OrbitControls(camera, renderer.domElement)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    canvas.appendChild(renderer.domElement)
+    camera.position.z = 0.3
+    controls.update()
+    const light = new THREE.AmbientLight(0xffffff, 0.5)
+    scene.add(light)
+    const light2 = new THREE.PointLight(0xffffff, 0.5)
+    scene.add(light2)
+    function loadGLTF() {
+      let object = new GLTFLoader()
+      object.load(
+        "./Schnauzer.glb",
+        function (gltf) {
+          scene.add(gltf.scene)
+        },
+        function (error) {
+          console.error(error)
+        }
+      )
+    }
+    loadGLTF()
+    const animate = () => {
+      requestAnimationFrame(animate)
+      renderer.render(scene, camera)
+    }
+    animate()
+  }, [])
+
+  return (
+    <div className="container-canvas">
+      <div ref={canvasRef} className="canvas"></div>
+    </div>
+  )
+}
+
+export default Object
