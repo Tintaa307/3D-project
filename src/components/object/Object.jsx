72899c9c6d344ff2255a339dc911c18f109b109d
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-11-11 02:32:45 -0300

    aniamtion fixed

--- src/components/object/Object.jsx
+++ src/components/object/Object.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useRef, useEffect } from "react"
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
-import { OBJLoader } from "three/addons/loaders/OBJLoader.js"
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
-import "./object.css"
+import { AnimationMixer, LoopRepeat } from "three"
 
 const Object = () => {
   const canvasRef = useRef(null)
   const scene = new THREE.Scene()
+  let light
+  scene.background = new THREE.Color(0x13131a)
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -23,24 +24,45 @@ const Object = () => {
     canvas.appendChild(renderer.domElement)
     camera.position.z = 0.3
     controls.update()
-    const light = new THREE.AmbientLight(0xffffff, 0.5)
+    scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000)
+    light = new THREE.HemisphereLight(0xffffff, 0x444444)
+    light.position.set(0, 200, 0)
     scene.add(light)
-    const light2 = new THREE.PointLight(0xffffff, 0.5)
-    scene.add(light2)
-    function loadGLTF() {
-      let object = new GLTFLoader()
-      object.load(
-        "./Schnauzer.glb",
-        function (gltf) {
-          scene.add(gltf.scene)
-        },
-        function (error) {
-          console.error(error)
-        }
-      )
-    }
-    loadGLTF()
+    light = new THREE.DirectionalLight(0xffffff)
+    light.position.set(0, 200, 100)
+    light.castShadow = true
+    light.shadow.camera.top = 180
+    light.shadow.camera.bottom = -100
+    light.shadow.camera.left = -120
+    light.shadow.camera.right = 120
+    scene.add(light)
+
+    let object = new GLTFLoader()
+    let mixer
+    object.load(
+      "/chicamodelo/scene.gltf",
+      function (gltf) {
+        let modelGltf = gltf.scene
+        modelGltf.scale.set(0.1, 0.1, 0.1)
+        scene.add(modelGltf)
+        mixer = new THREE.AnimationMixer(modelGltf)
+        const clips = gltf.animations
+        clips.forEach((clip) => {
+          mixer.clipAction(clip).play()
+        })
+      },
+      function (onProgress) {
+        console.log(onProgress)
+      },
+      function (error) {
+        console.log(error)
+      }
+    )
+    const clock = new THREE.Clock()
     const animate = () => {
+      if (mixer) {
+        mixer.update(clock.getDelta())
+      }
       requestAnimationFrame(animate)
       renderer.render(scene, camera)
     }
